4421c4f665501c48c2943e0ce4fe1b9ff9c31f5a
RianTavares <rian.tavares@live.com>
2023-03-25 03:55:30 -0300

    set theming for mac mask and start about me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -5,23 +5,15 @@ export const AboutMe = () => {
     return (
         <section className={styles.aboutMe}>
             <div className={styles.fadeoutWrapper}></div>
-                Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.Delegadis gente finis, bibendum egestas augue arcu ut est.Per aumento de cachacis, eu reclamis.Atirei o pau no gatis, per gatis num morreus.
-                    
-                Paisis, filhis, espiritis santis.Suco de cevadiss deixa as pessoas mais interessantis.Sapien in monti palavris qui num significa nadis i pareci latim.Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.
-                
-                In elementis mé pra quem é amistosis quis leo.Cevadis im ampola pa arma uma pindureta.Suco de cevadiss deixa as pessoas mais interessantis.Quem num gosta di mim que vai caçá sua turmis!
-                
-                Sapien in monti palavris qui num significa nadis i pareci latim.Interagi no mé, cursus quis, vehicula ac nisi.Quem num gosta di mé, boa gentis num é.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.
-                
-                Quem manda na minha terra sou euzis!Viva Forevis aptent taciti sociosqu ad litora torquent.Atirei o pau no gatis, per gatis num morreus.Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!
-                
-                Quem num gosta di mim que vai caçá sua turmis!Não sou faixa preta cumpadi, sou preto inteiris, inteiris.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Delegadis gente finis, bibendum egestas augue arcu ut est.
-                
-                Leite de capivaris, leite de mula manquis sem cabeça.Aenean aliquam molestie leo, vitae iaculis nisl.Não sou faixa preta cumpadi, sou preto inteiris, inteiris.Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.
-                
-                Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Quem manda na minha terra sou euzis!Detraxit consequat et quo num tendi nada.Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
-                
-                Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.Per aumento de cachacis, eu reclamis.Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.Cevadis im ampola pa arma uma pindureta.
+            <section className={styles.aboutMeInfoWrapper}>
+                <h1 className={styles.title}>OLÁ,</h1>
+                <p className={styles.text}>
+                    me chamo Rian Tavares. Sou desenvolvedor de software nascido e criado no Rio de Janeiro e baseado, 
+                    atualmente em Munique, Alemanha. Tenho aproximadamente 7 anos de experiência. Sou um entusiasta do mundo UI/UX, 
+                    sou mentor nas horas vagas e sonho em ajudar as pessoas utilizando meus conhecimentos.
+                </p>
+            </section>
+
         </section>
     )
 }
